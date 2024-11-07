7214570cd5c2fe6c9610e257e0c7665acf0e500c
kaylie <wowitskaylie@gmail.com>
2024-11-06 22:51:24 -0500

    Fuck

--- mods/RingsABell/main.gd
+++ mods/RingsABell/main.gd
@@ -14,6 +14,9 @@ func _ready():
 
 func _setup_plr(player: Node):
 	print("Here we go")
+	if player.name != "player":
+		print("Player doesn't seem to be you - skipping")
+		return
 	if player.get_node_or_null("sound_manager") == null:
 		print("We have no sound manager on this Player for some reason")
 		return
